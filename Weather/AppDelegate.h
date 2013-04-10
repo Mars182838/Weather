bc2d0901f0c9a8d9f16e1576a50eb3977d1a187c
Mars182838 <wj182838@126.com>
2013-04-10 10:21:04 +0800

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Mars on 13-4-10.
+//  Copyright (c) 2013年 Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
